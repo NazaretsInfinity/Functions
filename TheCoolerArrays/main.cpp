49be885294af0ce3609ad0d375003a7017e3af0c
Nazarets <ksenianazarec995@mail.com>
2024-03-27 17:50:19 +0300

    Overload  else for char and double

--- TheCoolerArrays/main.cpp
+++ TheCoolerArrays/main.cpp
@@ -1,52 +1,29 @@
 #include<iostream>
 using namespace std;
-void FillRand(int arr[],int const n=1);
-void Print(int arr[],int const n=1);
-int Sum(int arr[], int const n = 1, int sum=0);
-double Avg(int arr[], int const n=1, int sum=0);
-int minvaluein(int arr[], int const n=1);
-int maxvaluein(int arr[], int const n=1);
-void main() // checking ;3
+#define tab "\t"
+
+void FillRand(int arr[], const int n, int minRand = 0, int maxRand = 100);
+void main()
 {
-	int const s = 7;
-	int mass[s];
-	FillRand(mass,s);
-	Print(mass,s);
-	cout << endl;
-	cout << Sum(mass,s) << endl;
-	cout << Avg(mass,s) << endl;
-	cout << minvaluein(mass,s) << endl;
-	cout << maxvaluein(mass,s);
-} // done checking
-void FillRand(int arr[], int const n)
-{
-    for (int i=0; i < n; i++)arr[i] = rand();
+    const int m = 6;
+    int brr[m];
+    FillRand(brr, m);  
 }
-void Print(int arr[], int const n)
+void FillRand(int arr[], const int n, int minRand, int maxRand)
 {
-	for (int i = 0; i < n; i++)cout << arr[i] << "\t";
+    if (maxRand < minRand)
+    {
+        int buffer = minRand;
+        minRand = maxRand;
+        maxRand = buffer;
+    }
+    minRand *= 100;
+    maxRand *= 100;
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = minRand + rand() % (maxRand - minRand);
+        arr[i] /= 100;
+    }
 }
-int Sum(int arr[],int const n, int sum)
-{
-	for (int i = 0; i < n; i++)sum += arr[i];
-	return sum;
-}
-double Avg(int arr[], int const n,int sum)
-{
-	double avg;
-	for (int i = 0; i < n; i++)sum += arr[i];
-	return avg = (double)sum / n;
-}
-int minvaluein(int arr[], int const n)
-{
-	int min = arr[0];
-	for (int i = 0; i < n; i++)if (arr[i] < min)min = arr[i];
-	return min;
-}
-int maxvaluein(int arr[], int const n)
-{
-	int max = arr[0];
-	for (int i = 0; i < n; i++)if (arr[i] > max)max = arr[i];
-	return max;
-}
+
 
--- dostuff/main.cpp
+++ dostuff/main.cpp
@@ -1,24 +1,194 @@
 #include<iostream>
 using namespace std;
+#define tab "\t"
+void fillrand(int arr[], int const n, int minrand=0, int maxrand=100);
+void fillrand(double arr[], int const n, int minrand=0, int maxrand=1000); // double
+void fillrand(char arr[], int const n, int minrand=0, int maxrand=100); // char
+void print(const int arr[], const int n);
+void print(const double arr[], const int n);// double
+void print(const char arr[], const int n); // char
+int sum(const int arr[], const int n);
+double sum(const double arr[], const int n); // double
+int sum(const char arr[], const int n); // char
+double avg(const int arr[], const int n);
+double avg(const double arr[], const int n); // double
+double avg(const char arr[], const int n); // char
+void shiftleft(int arr[],const int n, int c);
+void shiftleft(double arr[],const int n, int c); // double
+void shiftleft(char arr[],const int n, int c); // char
+void shiftright(int arr[],const int n, int c);
+void shiftright(double arr[],const int n, int c); // double
+void shiftright(char arr[],const int n, int c); // char
 void main()
 {
-	const int n = 5;
-	int arr[n];
-	int minrand, maxrand;
-	do
-	{
-		cout << "Min is "; cin >> minrand;
-		cout << "Max is "; cin  >> maxrand;
-		if (minrand == maxrand)cout << "Let's start over ,Misha, it's all lame." << endl;
-	} while (minrand == maxrand);
-	//if (maxrand <= minrand)maxrand, minrand = minrand, maxrand;
-	if (maxrand <= minrand)
+	const int n = 7;
+	//double arr[n];
+	double arr[n];
+	/*int minrand, maxrand;
+	cin >> minrand >> maxrand;*/
+	fillrand(arr,n);
+	print(arr, n);
+	cout << "sum of the list - " << sum(arr, n) << endl;
+	cout << "Average is - " << avg(arr, n) << endl;
+	int c;
+	cout << "Size of shift: "; cin >> c;
+	shiftleft(arr, n, c);
+	shiftright(arr, n, c);
+}
+// int 
+void fillrand(int arr[], int const n, int minrand, int maxrand)
+{
+	if (maxrand < minrand)
 	{
 		int buffer = minrand;
 		minrand = maxrand;
 		maxrand = buffer;
 	}
-	for (int i = 0; i < n; i++)arr[i] =minrand + rand() % (maxrand-minrand);
-	for (int i = 0; i < n; i++)cout << arr[i] << "\t";
-	cout << endl;
+	for (int i = 0; i < n; i++)arr[i] = minrand + rand() % (maxrand - minrand);
+}
+void print(const int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)cout << arr[i] << tab; cout << endl;
+}
+int sum(const int arr[], const int n)
+{
+	int sum=0;
+	for (int i = 0; i < n; i++)sum += arr[i];
+	return sum;
+}
+double avg(const int arr[], const int n)
+{
+	return (double)sum(arr, n) / n;
+}
+void shiftleft(int arr[], const int n, int c)
+{
+	for (int i = 0; i < c; i++)
+	{
+		int buffer = arr[0];
+		for (int l = 0; l < n-1; l++)
+		{
+			arr[l] = arr[l + 1];
+		}
+		arr[n - 1] = buffer;
+	}
+	print(arr, n);
+}
+void shiftright(int arr[], const int n, int c)
+{
+		for (int i = 0; i < c; i++)
+		{
+			int buffer = arr[n-1];
+			for (int l = n-1; l > 0; l--)
+			{
+				arr[l] = arr[l-1];
+			}
+			arr[0] = buffer;
+		}
+		print(arr, n);
+}
+// double
+void fillrand(double arr[], int const n, int minrand, int maxrand)
+{
+	if (maxrand < minrand)
+	{
+		int buffer = minrand;
+		minrand = maxrand;
+		maxrand = buffer;
+	}
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = minrand + rand() % (maxrand - minrand);
+		arr[i] /= 100;
+	}
+}
+void print(const double arr[], const int n)
+{
+	for (int i = 0; i < n; i++)cout << arr[i] << tab; cout << endl;
+}
+double sum(const double arr[], const int n)
+{
+	 double sum = 0;
+	for (int i = 0; i < n; i++)sum += arr[i];
+	return sum;
+}
+double avg(const double arr[], const int n)
+{
+	return (double)sum(arr, n) / n;
+}
+void shiftleft(double arr[], const int n, int c)
+{
+	for (int i = 0; i < c; i++)
+	{
+		double buffer = arr[0];
+		for (int l = 0; l < n - 1; l++)
+		{
+			arr[l] = arr[l + 1];
+		}
+		arr[n - 1] = buffer;
+	}
+	print(arr, n);
+}
+void shiftright(double arr[], const int n, int c)
+{
+	for (int i = 0; i < c; i++)
+	{
+		double buffer = arr[n - 1];
+		for (int l = n - 1; l > 0; l--)
+		{
+			arr[l] = arr[l - 1];
+		}
+		arr[0] = buffer;
+	}
+	print(arr, n);
+}
+//char
+void fillrand(char arr[], int const n, int minrand, int maxrand)
+{
+	if (maxrand < minrand)
+	{
+		int buffer = minrand;
+		minrand = maxrand;
+		maxrand = buffer;
+	}
+	for (int i = 0; i < n; i++)arr[i] = minrand + rand() % (maxrand - minrand);
+}
+void print(const char arr[], const int n)
+{
+	for (int i = 0; i < n; i++)cout << arr[i] << tab; cout << endl;
+}
+int sum(const char arr[], const int n)
+{
+	int sum = 0;
+	for (int i = 0; i < n; i++)sum += arr[i];
+	return sum;
+}
+double avg(const char arr[], const int n)
+{
+	return (double)sum(arr, n) / n;
+}
+void shiftleft(char arr[], const int n, int c)
+{
+	for (int i = 0; i < c; i++)
+	{
+		int buffer = arr[0];
+		for (int l = 0; l < n - 1; l++)
+		{
+			arr[l] = arr[l + 1];
+		}
+		arr[n - 1] = buffer;
+	}
+	print(arr, n);
+}
+void shiftright(char arr[], const int n, int c)
+{
+	for (int i = 0; i < c; i++)
+	{
+		double buffer = arr[n - 1];
+		for (int l = n - 1; l > 0; l--)
+		{
+			arr[l] = arr[l - 1];
+		}
+		arr[0] = buffer;
+	}
+	print(arr, n);
 }
