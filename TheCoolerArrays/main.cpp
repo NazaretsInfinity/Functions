5609f973a8b759a9c44a0ba1de17d12305a453bc
Nazarets <ksenianazarec995@mail.com>
2024-04-19 19:17:36 +0300

    tried a search

--- TheCoolerArrays/main.cpp
+++ TheCoolerArrays/main.cpp
@@ -6,9 +6,10 @@ int Sum(int arr[], int const n = 1, int sum=0);
 double Avg(int arr[], int const n=1, int sum=0);
 int minvaluein(int arr[], int const n=1);
 int maxvaluein(int arr[], int const n=1);
+void search(int arr[], int const);
 void main() // checking ;3
 {
-	int const s = 7;
+	int const s = 15;
 	int mass[s];
 	FillRand(mass,s);
 	Print(mass,s);
@@ -16,11 +17,12 @@ void main() // checking ;3
 	cout << Sum(mass,s) << endl;
 	cout << Avg(mass,s) << endl;
 	cout << minvaluein(mass,s) << endl;
-	cout << maxvaluein(mass,s);
+	cout << maxvaluein(mass,s) << endl;
+	search(mass, s);
 } // done checking
 void FillRand(int arr[], int const n)
 {
-    for (int i=0; i < n; i++)arr[i] = rand();
+    for (int i=0; i < n; i++)arr[i] = rand()%5;
 }
 void Print(int arr[], int const n)
 {
@@ -49,4 +51,28 @@ int maxvaluein(int arr[], int const n)
 	for (int i = 0; i < n; i++)if (arr[i] > max)max = arr[i];
 	return max;
 }
+void search(int arr[], int const n)
+{
+
+	for (int i = 0; i < n; i++)
+	{
+		int same = 1;
+		int check = 0;
+		for (int l = i - 1; l >= 0; l--)if (arr[i] == arr[l])
+		{
+			check++;
+			break;
+		}
+		if (check == 0)
+		{
+			for (int j = 0; j < n; j++)if (arr[i] == arr[j] && j != i)
+				{
+					cout << arr[j] << " ";
+					same += 1;
+				}
+		if (same > 1)cout << arr[i] << " amount of repeated numbers: " << same << endl;
+		}
+	}
+}
+
 
